7ef588050fd22752fdde19698516e1d61cf6d320
Pramod Thomas <pmod23@gmail.com>
2020-05-21 16:20:33 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -5,13 +5,17 @@ end
 
 def my_hash
   hash = {1 => "hello"}
+  hash
 end
 
 def pioneer
-  
+  hash = {:name => "Grace Hopper"}
+  hash
 end
 
 
 def id_hash_generator(number)
-  # return a hash with a key :id assigned to the provided number
+  hash = {}
+  hash[:id] = number
+  hash
 end
